8741f1e3b9649020b25d32d0a2bdadcdb0645916
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-08-01 08:27:41 +0300

    Initial Commit

---
+++ ToDoList_SwiftUI_Firebase/ToDoList_SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUI_FirebaseApp.swift
+//  ToDoList_SwiftUI_Firebase
+//
+//  Created by Kate Kashko on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
