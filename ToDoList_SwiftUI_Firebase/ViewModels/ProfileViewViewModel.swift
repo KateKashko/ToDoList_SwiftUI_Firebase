bf5c443961a92361b529aa35c5e78e6701857421
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-08-03 11:46:52 +0300

    Checkmarks done

--- ToDoList_SwiftUI_Firebase/ViewModels/ProfileViewViewModel.swift
+++ ToDoList_SwiftUI_Firebase/ViewModels/ProfileViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Kate Kashko on 1.08.2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
@@ -12,6 +13,19 @@ class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
 }
--- ToDoList_SwiftUI_Firebase/Views/ToDoListItemView.swift
+++ ToDoList_SwiftUI_Firebase/Views/ToDoListItemView.swift
@@ -29,6 +29,7 @@ struct ToDoListItemView: View {
                 
             } label: {
                 Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(.green)
             }
         }
     }
