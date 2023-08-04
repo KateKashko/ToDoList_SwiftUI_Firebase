270f6e4dba8dda0cb79e3cdc38239c4122a7c791
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-08-04 14:38:15 +0300

    Profil, logout  and icon

--- ToDoList_SwiftUI_Firebase/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList_SwiftUI_Firebase/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,9 +4,27 @@
 //
 //  Created by Kate Kashko on 1.08.2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+        
+    }
 }
--- ToDoList_SwiftUI_Firebase/Views/ProfileView.swift
+++ ToDoList_SwiftUI_Firebase/Views/ProfileView.swift
@@ -16,11 +16,57 @@ struct ProfileView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+   
+                } else {
+                    Text ("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
-         }
-     }
+        }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+        .padding()
+    }
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.black)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        //Info
+        Form {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Name: ")
+                    Text(user.name)
+                }
+                HStack {
+                    Text("Email: ")
+                    Text(user.email)
+                }
+                HStack {
+                    Text("Member Since: ")
+                    Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                }
+            }
+            .padding()
+            TLButton(
+                title: "Log Out",
+                background: .red
+            ) {
+                viewModel.logOut()
+            }
+            .padding()
+        }
+        Spacer()
+    }
 }
 
 struct ProfileView_Previews: PreviewProvider {
